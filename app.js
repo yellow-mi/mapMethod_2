a703dfcd1cbf03edb9506168709bb27fa1ecc72e
yellow-mi <61219261+yellow-mi@users.noreply.github.com>
2022-03-16 18:47:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Imperative Code
+const people = ['Amanda', 'Giovanna', 'Alfred', 'Connor'];
+const newArr = [];
+
+for (let i = 0; i < people.length; i++) {
+  newArr[i] = people[i] + 'I\'m in a new Array, wow!'
+}
+console.log(newArr);
+
+// Declerative Code
